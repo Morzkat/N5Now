299a2515a2835e84ab5905b80235163f88df0b75
Morzkat <felix.david21@outlook.com>
2024-09-08 16:50:12 -0400

    * Add missing pagination class

---
+++ Backend/src/N5Now.Domain/Common/Pagination.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N5Now.Domain.Common
+{
+    public class Pagination
+    {
+        public int Skip { get; set; } = 0;
+        public int Limit { get; set; } = 10;
+    }
+}
